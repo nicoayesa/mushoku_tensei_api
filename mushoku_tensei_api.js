7177bf10dc6cf3e09749d96a200c433d052e6b34
nicoayesa <148084343+nicoayesa@users.noreply.github.com>
2023-11-13 19:41:37 +0100

    Create mushoku_tensei_api.js

---
+++ mushoku_tensei_api.js
@@ -0,0 +1,122 @@
+[
+ {
+  "id": 1,
+  "nombre": "Rudeus Greyrat",
+  "edad": 34,
+  "raza": "Humano",
+  "estilo": "Magia",
+  "imagen": "https://static.wikia.nocookie.net/mushokutensei/images/e/ec/MT-AN-S2-Rudeus-Uniform-FullBody.png/revision/latest?cb=20230804000750"
+ },
+ {
+  "id": 2,
+  "nombre": "Roxy Migurdia",
+  "edad": 27,
+  "raza": "Elfo",
+  "estilo": "Magia",
+  "imagen": "https://static.wikia.nocookie.net/mushokutensei/images/e/e5/RoxyAnime.png/revision/latest?cb=20200709063823"
+ },
+ {
+  "id": 3,
+  "nombre": "Sylphiette",
+  "edad": 18,
+  "raza": "Bestia mágica",
+  "estilo": "Magia",
+  "imagen": "https://static.wikia.nocookie.net/mushokutensei/images/5/5d/MT-AN-S2-Fitts-FullBody.png/revision/latest?cb=20230804000822"
+ },
+ {
+  "id": 4,
+  "nombre": "Ghislaine Dedoldia",
+  "edad": 27,
+  "raza": "Bestia mágica",
+  "estilo": "Espada",
+  "imagen": "url_imagen_ghislaine"
+ },
+ {
+  "id": 5,
+  "nombre": "Paul Greyrat",
+  "edad": 45,
+  "raza": "Humano",
+  "estilo": "Espada",
+  "imagen": "url_imagen_paul"
+ },
+ {
+  "id": 6,
+  "nombre": "Zenith Greyrat",
+  "edad": 38,
+  "raza": "Humano",
+  "estilo": "Magia",
+  "imagen": "url_imagen_zenith"
+ },
+ {
+  "id": 7,
+  "nombre": "Lilia Dedoldia",
+  "edad": 25,
+  "raza": "Humano",
+  "estilo": "Magia",
+  "imagen": "url_imagen_lilia"
+ },
+ {
+  "id": 8,
+  "nombre": "Norn Greyrat",
+  "edad": 15,
+  "raza": "Humano",
+  "estilo": "Magia",
+  "imagen": "url_imagen_norn"
+ },
+ {
+  "id": 9,
+  "nombre": "Aisha Greyrat",
+  "edad": 28,
+  "raza": "Humano",
+  "estilo": "Espada",
+  "imagen": "url_imagen_aisha"
+ },
+ {
+  "id": 10,
+  "nombre": "Orsted",
+  "edad": "Desconocida",
+  "raza": "Desconocida",
+  "estilo": "Desconocido",
+  "imagen": "url_imagen_orsted"
+ },
+ {
+  "id": 11,
+  "nombre": "Hitogami",
+  "edad": "Desconocida",
+  "raza": "Desconocida",
+  "estilo": "Desconocido",
+  "imagen": "url_imagen_hitogami"
+ },
+ {
+  "id": 12,
+  "nombre": "Dead End",
+  "edad": "Desconocida",
+  "raza": "Desconocida",
+  "estilo": "Desconocido",
+  "imagen": "url_imagen_dead_end"
+ },
+ {
+  "id": 13,
+  "nombre": "Ruijerd Superdia",
+  "edad": "Desconocida",
+  "raza": "Super humano",
+  "estilo": "Espada",
+  "imagen": "url_imagen_ruijerd"
+ },
+ {
+  "id": 14,
+  "nombre": "Tina",
+  "edad": 24,
+  "raza": "Humano",
+  "estilo": "Magia",
+  "imagen": "url_imagen_tina"
+ },
+ {
+  "id": 15,
+  "nombre": "Eris Boreas Greyrat",
+  "edad": 32,
+  "raza": "Humano",
+  "estilo": "Espada",
+  "imagen": "url_imagen_eris"
+ }
+]
